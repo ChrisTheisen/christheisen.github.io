c7ffc52051a07a8087943e5592a417a785beedc1
Chris Theisen <grumdrig333@gmail.com>
2025-01-01 08:17:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function setActive(input){
   Array.from(a).forEach(x => x.classList.toggle('hide', true));
   document.getElementById(input).classList.toggle('hide', false);
 }
+
+function init(){
+  const buttons = Array.from(document.getElementsByClassName('mnuButton'));
+  buttons.forEach(x => {
+    const id = x.id.replace('mnu','div'); 
+    console.log(id);
+    x.addEventListener('click', () => setActive(id));
+  });
+}
+
+init();
