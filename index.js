d31b13f417d0873e895de4f0dc752f1429c0d6fd
Chris Theisen <grumdrig333@gmail.com>
2025-01-01 13:41:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function setActive(input){
   const a = document.getElementsByClassName('content');
-  Array.from(a).forEach(x => x.classList.toggle('hide', true));
-  document.getElementById(input).classList.toggle('hide', false);
+  Array.from(a).forEach(x => x.classList.toggle('hide', true);
+  document.getElementById(input.id.replace('mnu','div')).classList.toggle('hide', false);
+
+  const b = document.getElementsByClassName('mnuButton');
+  Array.from(b).forEach(x => x.classList.toggle('activeButton', false));
+  input.classList.toggle('activeButton', true);
 }
 
 function init(){
   const buttons = Array.from(document.getElementsByClassName('mnuButton'));
   buttons.forEach(x => {
-    const id = x.id.replace('mnu','div'); 
-    x.addEventListener('click', () => setActive(id));
+    x.addEventListener('click', () => setActive(this));
   });
 }
 
