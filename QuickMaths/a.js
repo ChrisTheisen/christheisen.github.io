a754bdf1df561b17d6edde1b98a9555726cf30cd
Chris Theisen <grumdrig333@gmail.com>
2025-03-15 11:13:26 -0500

    Update a.js

--- QuickMaths/a.js
+++ QuickMaths/a.js
@@ -170,7 +170,7 @@ function incorrect(){
 }
 
 function checkAnswer(){
-	if(!ans.value.trim() === ''){return;}
+	if(!ans.value.trim()){return;}
 	const x = Number(ans.value);
 	eq.classList.remove('wrong');
 
